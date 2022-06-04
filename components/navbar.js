64d1f3dd890555e4a0823b537543bdfdc9de9d23
Henry Betancourth <henrybetancourth@ERROR-404.fios-router.home>
2022-06-03 20:23:17 -0400

    update

--- components/navbar.js
+++ components/navbar.js
@@ -102,10 +102,7 @@ const Navbar = (props) => {
 								<NextLink href="/works" passHref>
 									<MenuItem as={Link}>Works</MenuItem>
 								</NextLink>
-								<NextLink href="/posts" passHref>
-									<MenuItem as={Link}>Posts</MenuItem>
-								</NextLink>
-								<MenuItem as={Link} href="/404">
+								<MenuItem as={Link} href="https://github.com/noelys215">
 									GitHub
 								</MenuItem>
 							</MenuList>
